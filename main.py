81f4c06fab5fd86ec4dff7a7f34a8bb32fe7f100
cowbotic <mario.arienza@capside.com>
2017-05-24 12:26:21 +0200

    Commit inicial

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, request
+app = Flask(__name__)
+
+
+app.config.from_envvar('FLASK_GLOBAL')
+
+@app.route('/')
+def hello():
+  return render_template('hello.html')
+
+@app.route('/datos')
+def datos():
+    return render_template('datos.html')
+
+@app.route('/conecta')
+def conecta(methods=['GET', 'POST']):
+    if request.method=='POST':
+        account_name=int(request.form['account'])
+        db_name=int(request.form['db_name'])
+        coll_name=int(request.form['coll_name'])
+        master_key=int(request.form['master_key'])
+        lon=int(request.form['lon'])
+        lat=int(request.form['lat'])
+
+if __name__ == '__main__':
+  app.run()
